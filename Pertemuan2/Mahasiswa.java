f26dc6092bfb4cf0f48716b16e455726298f015e
Rizkygunardi <rgunardi97@gmail.com>
2017-02-28 19:22:55 +0700

    Add files via upload

---
+++ Pertemuan2/Mahasiswa.java
@@ -0,0 +1,8 @@
+public class Mahasiswa {
+
+	public static void main(String[] args){
+		System.out.println("Nama   : Rizky Gunardi");
+		System.out.println("NIM	   : 10115104");
+		System.out.println("Alamat : Dusun Cicelot rt04 / rw07 Desa Mandalaherang Kec.Cimalaka Kab.Sumedang");
+	}
+}
