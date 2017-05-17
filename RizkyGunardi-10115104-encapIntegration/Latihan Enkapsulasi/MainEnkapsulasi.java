6668450cca6e0f74ab479b4cc28598f439323de7
Rizkygunardi <rgunardi97@gmail.com>
2017-05-17 20:14:43 +0700

    Add files via upload

---
+++ RizkyGunardi-10115104-encapIntegration/Latihan Enkapsulasi/MainEnkapsulasi.java
@@ -0,0 +1,11 @@
+public class MainEnkapsulasi{
+	
+	public static void main(String[] args){
+	
+		enkapsulasi enka = new enkapsulasi();
+		enka.setOpsi1(10115104);
+		System.out.println(enka.getOpsi1());
+		enka.setOpsi2("Rizky Gunardi");
+		System.out.println(enka.getOpsi2());
+	}
+}
