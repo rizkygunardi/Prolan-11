4006880ca906eeabbcad722535a5b5ba12c62a00
Rizkygunardi <rgunardi97@gmail.com>
2017-07-04 23:09:01 +0700

    Add files via upload

---
+++ Pertemuan14lab/spesifikasi/tegangan.java
@@ -0,0 +1,14 @@
+package spesifikasi;
+
+public class tegangan <T>{
+            
+            private T tg_psu;
+		
+            public T gettg_psu(){
+                return tg_psu;
+            }
+		
+            public void settegangan(T tg_psu){
+		this.tg_psu = tg_psu;
+            }
+}
